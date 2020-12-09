6ca0eb54d06ec8134a4c1289064c7abea51bf9dd
houchengen <HCE19970702>
2020-12-09 11:16:27 +0800

    优化

--- src/main/java/com/ezio/processor/NetEaseMusicPageProcessor.java
+++ src/main/java/com/ezio/processor/NetEaseMusicPageProcessor.java
@@ -6,9 +6,11 @@ import com.alibaba.fastjson.JSONPath;
 import com.ezio.entity.Music;
 import com.ezio.entity.MusicComment;
 import com.ezio.entity.MusicCommentDto;
+import com.ezio.entity.NetEaseMusicEntity;
 import com.ezio.pipeline.NetEaseMusicPipeline;
 import com.ezio.service.MusicService;
 import com.ezio.utils.HttpUtils;
+import com.ezio.utils.NetEaseMusicEntityUtil;
 import com.ezio.utils.NetEaseMusicUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,12 +83,23 @@ public class NetEaseMusicPageProcessor implements PageProcessor {
             music.setTitle(page.getHtml().xpath("//em[@class='f-ff2']/text()").toString());
             music.setAuthor(page.getHtml().xpath("//p[@class='des s-fc4']/span/a/text()").toString());
             music.setAlbum(page.getHtml().xpath("//p[@class='des s-fc4']/a/text()").toString());
-            music.setURL(url.replace("http","https"));
-            music.setMp3url("https://music.163.com/song/media/outer/url?id=" + songId + ".mp3");
+            music.setURL(url.replace("http", "https"));
+//            music.setMp3url("https://music.163.com/song/media/outer/url?id=" + songId + ".mp3");
+            try {
+                NetEaseMusicEntity netEaseMusicEntity = NetEaseMusicEntityUtil.getNetEaseMusicEntity(songId);
+                if (netEaseMusicEntity != null && netEaseMusicEntity.getData() != null && netEaseMusicEntity.getData().size() > 0) {
+                    Object mp3url = netEaseMusicEntity.getData().get(0).getUrl();
+                    if (mp3url != null) {
+                        music.setMp3url(mp3url.toString());
+                    }
+                }
+
+            } catch (Exception e) {
+            }
             JSONObject jsonObject = JSONObject.parseObject(HttpUtils.httpGetRequest("https://music.163.com/api/song/media?id=" + songId));
-            if(jsonObject.get("lyric")==null){
+            if (jsonObject.get("lyric") == null) {
                 music.setLyric("");
-            }else {
+            } else {
                 music.setLyric(jsonObject.get("lyric").toString());
             }
 
